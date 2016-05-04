84a5c1e60230abae5fdc948874b298c4a002fbde
jkupcho <jonathan.kupcho@gmail.com>
2016-05-03 22:40:56 -0500

    more indentation fixing

--- ios/src/containers/App.js
+++ ios/src/containers/App.js
@@ -3,29 +3,29 @@ import React, { View, Navigator, Component, StyleSheet } from 'react-native';
 import HomeList from './HomeList'
 
 export default class App extends Component {
-	renderScene(route, navigator) {
-		const Component = route.component;
-		return (
-			<Component {...route.props} navigator={navigator} route={route} />
-			);
-	}
+  renderScene(route, navigator) {
+    const Component = route.component;
+    return (
+      <Component {...route.props} navigator={navigator} route={route} />
+    );
+  }
 
-	render () {
-		return (
-			<Navigator
-			initialRoute={{
-				component: HomeList,
-			}}
-			style={styles.container}
-			renderScene={this.renderScene}
-			/>
-			);
-	}
+  render () {
+    return (
+      <Navigator
+        initialRoute={{
+          component: HomeList,
+        }}
+        style={styles.container}
+        renderScene={this.renderScene}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'white',
-	}
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  }
 });
